0c1bc10dfeee36a178a95431dd3de5c9da8b958b
TheNodeDev <TheNodeDeveloper@gmail.com>
2023-02-10 07:22:56 +0000

    build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+if ! command -v nim &> /dev/null
+then
+    echo "<the_command> could not be found"
+    exit
+fi
+
+FILE=./build/cimgui.so
+if ! test -f "$FILE"; then
+    mkdir build
+    echo "cimgui not found, building..."
+    git clone --recursive https://github.com/nimgl/cimgui.git
+    cd cimgui
+    mkdir bld
+    cd bld
+    cmake ..
+    make
+    cp ./cimgui.so ../../build/cimgui.so
+    cd ../..
+fi
+
+nim c -d:ssl -d:release -o:./build/pp ./gui.nim
